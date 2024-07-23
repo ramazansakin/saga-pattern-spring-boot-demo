f4f96c44aceb440eccd9a880bbe8895652455c76
simplyi <sergey.kargopolov@gmail.com>
2024-07-23 17:07:36 -0400

    Reserve Product in Stock - business logic

--- products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
+++ products-service/src/main/java/com/appsdeveloperblog/products/service/handler/ProductCommandsHandler.java
@@ -1,6 +1,10 @@
 package com.appsdeveloperblog.products.service.handler;
 
+import com.appsdeveloperblog.core.dto.Product;
 import com.appsdeveloperblog.core.dto.commands.ReserveProductCommand;
+import com.appsdeveloperblog.products.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,8 +14,21 @@ import org.springframework.stereotype.Component;
 @KafkaListener(topics="${products.commands.topic.name}")
 public class ProductCommandsHandler {
 
+    private final ProductService productService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public ProductCommandsHandler(ProductService productService) {
+        this.productService = productService;
+    }
+
     @KafkaHandler
     public void handleCommand(@Payload ReserveProductCommand command) {
 
+        try {
+            Product desiredProduct = new Product(command.getProductId(), command.getProductQuantity());
+            Product reservedProduct = productService.reserve(desiredProduct, command.getOrderId());
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
     }
 }

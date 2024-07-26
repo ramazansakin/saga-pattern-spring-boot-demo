31b3fbe88293bbd7043e59fcac7261dcf866121b
simplyi <sergey.kargopolov@gmail.com>
2024-07-26 17:16:27 -0400

    Handle Process Payment Command - Business Logic

--- payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentsCommandsHandler.java
+++ payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentsCommandsHandler.java
@@ -1,6 +1,11 @@
 package com.appsdeveloperblog.payments.service.handler;
 
+import com.appsdeveloperblog.core.dto.Payment;
 import com.appsdeveloperblog.core.dto.commands.ProcessPaymentCommand;
+import com.appsdeveloperblog.core.exceptions.CreditCardProcessorUnavailableException;
+import com.appsdeveloperblog.payments.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,8 +15,24 @@ import org.springframework.stereotype.Component;
 @KafkaListener(topics="${payments.commands.topic.name}")
 public class PaymentsCommandsHandler {
 
+    private final PaymentService paymentService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public PaymentsCommandsHandler(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
     @KafkaHandler
     public void handleCommand(@Payload ProcessPaymentCommand command) {
 
+        try {
+            Payment payment = new Payment(command.getOrderId(),
+                    command.getProductId(),
+                    command.getProductPrice(),
+                    command.getProductQuantity());
+            Payment processedPayment = paymentService.process(payment);
+        } catch (CreditCardProcessorUnavailableException e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
     }
 }

e8f28f2f8128322d2b54d97cf64ba4d27385c6f2
simplyi <sergey.kargopolov@gmail.com>
2024-07-28 14:03:17 -0400

    Handle PaymentProcessedEvent

--- orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
@@ -17,6 +17,9 @@ public class KafkaConfig {
     private String productsCommandsTopicName;
     @Value("${payments.commands.topic.name}")
     private String paymentsCommandsTopicName;
+    @Value("${orders.commands.topic.name}")
+    private String ordersCommandsTopicName;
+
     private final static Integer TOPIC_REPLICATION_FACTOR=3;
     private final static Integer TOPIC_PARTITIONS=3;
 
@@ -44,6 +47,14 @@ public class KafkaConfig {
     @Bean
     NewTopic createPaymentsCommandsTopic() {
         return TopicBuilder.name(paymentsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createOrdersCommandsTopic() {
+        return TopicBuilder.name(ordersCommandsTopicName)
                 .partitions(TOPIC_PARTITIONS)
                 .replicas(TOPIC_REPLICATION_FACTOR)
                 .build();

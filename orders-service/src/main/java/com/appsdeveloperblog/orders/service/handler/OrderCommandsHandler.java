27c07577a16e7d32eec225f3c71dfd54d77b2617
simplyi <sergey.kargopolov@gmail.com>
2024-07-31 17:30:57 -0400

    Handle the RejectOrderCommand

--- orders-service/src/main/java/com/appsdeveloperblog/orders/service/handler/OrderCommandsHandler.java
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/service/handler/OrderCommandsHandler.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.orders.service.handler;
 
 import com.appsdeveloperblog.core.dto.commands.ApproveOrderCommand;
+import com.appsdeveloperblog.core.dto.commands.RejectOrderCommand;
 import com.appsdeveloperblog.orders.service.OrderService;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -20,5 +21,10 @@ public class OrderCommandsHandler {
     @KafkaHandler
     public void handleCommand(@Payload ApproveOrderCommand approveOrderCommand) {
         orderService.approveOrder(approveOrderCommand.getOrderId());
+    }
+
+    @KafkaHandler
+    public void handleCommand(@Payload RejectOrderCommand rejectOrderCommand) {
+        orderService.rejectOrder(rejectOrderCommand.getOrderId());
     }
 }

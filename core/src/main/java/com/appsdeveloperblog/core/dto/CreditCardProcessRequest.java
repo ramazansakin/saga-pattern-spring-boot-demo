f51593d5d7310f6558b79136011e065c07ea8c38
rsakin <ramazansakin63@gmail.com>
2025-03-29 09:17:02 +0300

    feat: event models up as record and adapted

--- core/src/main/java/com/appsdeveloperblog/core/dto/CreditCardProcessRequest.java
+++ core/src/main/java/com/appsdeveloperblog/core/dto/CreditCardProcessRequest.java
@@ -6,35 +6,8 @@ import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-public class CreditCardProcessRequest {
-    @NotNull
-    @Positive
-    private BigInteger creditCardNumber;
-    @NotNull
-    @Positive
-    private BigDecimal paymentAmount;
-
-    public CreditCardProcessRequest() {
-    }
-
-    public CreditCardProcessRequest(BigInteger creditCardNumber, BigDecimal paymentAmount) {
-        this.creditCardNumber = creditCardNumber;
-        this.paymentAmount = paymentAmount;
-    }
-
-    public BigInteger getCreditCardNumber() {
-        return creditCardNumber;
-    }
-
-    public void setCreditCardNumber(BigInteger creditCardNumber) {
-        this.creditCardNumber = creditCardNumber;
-    }
-
-    public BigDecimal getPaymentAmount() {
-        return paymentAmount;
-    }
-
-    public void setPaymentAmount(BigDecimal paymentAmount) {
-        this.paymentAmount = paymentAmount;
-    }
+public record CreditCardProcessRequest(
+        @NotNull BigInteger cardNumber,
+        @NotNull @Positive BigDecimal paymentAmount
+) {
 }
--- orders-service/src/main/java/com/appsdeveloperblog/orders/saga/OrderSaga.java
+++ orders-service/src/main/java/com/appsdeveloperblog/orders/saga/OrderSaga.java
@@ -12,7 +12,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(topics={
+@KafkaListener(topics = {
         "${orders.events.topic.name}",
         "${products.events.topic.name}",
         "${payments.events.topic.name}"
@@ -41,41 +41,45 @@ public class OrderSaga {
     public void handleEvent(@Payload OrderCreatedEvent event) {
 
         ReserveProductCommand command = new ReserveProductCommand(
-                event.getProductId(),
-                event.getProductQuantity(),
-                event.getOrderId()
+                event.productId(),
+                event.productQuantity(),
+                event.orderId()
         );
 
-        kafkaTemplate.send(productsCommandsTopicName,command);
-        orderHistoryService.add(event.getOrderId(), OrderStatus.CREATED);
+        kafkaTemplate.send(productsCommandsTopicName, command);
+        orderHistoryService.add(event.orderId(), OrderStatus.CREATED);
     }
 
     @KafkaHandler
     public void handleEvent(@Payload ProductReservedEvent event) {
 
-        ProcessPaymentCommand processPaymentCommand = new ProcessPaymentCommand(event.getOrderId(),
-                event.getProductId(),event.getProductPrice(), event.getProductQuantity());
-        kafkaTemplate.send(paymentsCommandsTopicName,processPaymentCommand);
+        ProcessPaymentCommand processPaymentCommand = new ProcessPaymentCommand(event.orderId(),
+                event.productId(), event.productPrice(), event.productQuantity());
+
+        kafkaTemplate.send(paymentsCommandsTopicName, processPaymentCommand);
     }
 
     @KafkaHandler
     public void handleEvent(@Payload PaymentProcessedEvent event) {
 
-        ApproveOrderCommand approveOrderCommand = new ApproveOrderCommand(event.getOrderId());
-        kafkaTemplate.send(ordersCommandsTopicName,approveOrderCommand);
+        ApproveOrderCommand approveOrderCommand = new ApproveOrderCommand(event.orderId());
+        kafkaTemplate.send(ordersCommandsTopicName, approveOrderCommand);
     }
 
     @KafkaHandler
     public void handleEvent(@Payload OrderApprovedEvent event) {
-        orderHistoryService.add(event.getOrderId(), OrderStatus.APPROVED);
+        orderHistoryService.add(event.orderId(), OrderStatus.APPROVED);
     }
 
     @KafkaHandler
     public void handleEvent(@Payload PaymentFailedEvent event) {
+
         CancelProductReservationCommand cancelProductReservationCommand =
-                new CancelProductReservationCommand(event.getProductId(),
-                        event.getOrderId(),
-                        event.getProductQuantity());
+                new CancelProductReservationCommand(
+                        event.productId(),
+                        event.orderId(),
+                        event.productQuantity());
+
         kafkaTemplate.send(productsCommandsTopicName, cancelProductReservationCommand);
     }
 
@@ -83,7 +87,10 @@ public class OrderSaga {
     public void handleEvent(@Payload ProductReservationCancelledEvent event) {
 
         RejectOrderCommand rejectOrderCommand = new RejectOrderCommand(event.getOrderId());
+
         kafkaTemplate.send(ordersCommandsTopicName, rejectOrderCommand);
+
         orderHistoryService.add(event.getOrderId(), OrderStatus.REJECTED);
     }
+
 }
